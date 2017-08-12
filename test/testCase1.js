66a7d733f8ae9502560aeca14b1c4840807d9d38
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2017-08-12 23:40:11 +0300

    initial commit

---
+++ test/testCase1.js
@@ -0,0 +1,7 @@
+'use strict'
+
+import should from 'should/as-function'
+
+it('should be defined', async () => {
+  should.equal(true, true)
+})
---
+++ test/testCase2.js
@@ -0,0 +1,7 @@
+'use strict'
+
+import should from 'should/as-function'
+
+it('should be defined2', async () => {
+  should.equal(true, true)
+})
